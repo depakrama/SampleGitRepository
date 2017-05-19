d93dac0101ee9bef0235bec9aea9d01874da549a
depakrama <depakrama@yahoo.com>
2017-05-19 10:00:58 +1000

    Update StringUtil.java

--- StringUtil.java
+++ StringUtil.java
@@ -9,31 +9,34 @@ public class StringUtil {
 		String truncatedStr = "";
 		int truncatedLength = TRUN_STRING.length();
 		int strLength = 0;
+		boolean canBeTruncated = false;
 		if(StringUtils.isNotBlank(termsAndConditions)){
 			strLength = termsAndConditions.length();
+			canBeTruncated = true;
 		}
-		
-		//if string length is less than the max length , then return whole string
-		if(strLength <= maxLength){
-			truncatedStr = termsAndConditions;
-		}else if(strLength > maxLength){
-			//if the maxLength is less than truncate constant, then return the whole string
-			if(truncatedLength >= maxLength){
+		//truncate has to happen only when the string is not null
+		if(canBeTruncated && maxLength > 0){
+			//if string length is less than the max length , then return whole string
+			if(strLength <= maxLength){
 				truncatedStr = termsAndConditions;
-			}else if(truncatedLength < maxLength){//if the maxlength is greater than truncate constant,then truncate the string 
-				int toBeTruncated = (maxLength - truncatedLength) / 2 ;
-				int modCount = (maxLength - truncatedLength) % 2 ;
-				
-				StringBuilder strBuilder = new StringBuilder();
-				if(modCount == 0){
-				strBuilder.append(termsAndConditions.substring(0, toBeTruncated)).append(TRUN_STRING).append(termsAndConditions.substring((strLength-toBeTruncated),strLength));
-				}else{
-					strBuilder.append(termsAndConditions.substring(0, toBeTruncated)).append(TRUN_STRING).append(termsAndConditions.substring((strLength-(toBeTruncated+modCount)),strLength));
+			}else if(strLength > maxLength){
+				//if the maxLength is less than truncate constant, then return the whole string
+				if(truncatedLength >= maxLength){
+					truncatedStr = termsAndConditions;
+				}else if(truncatedLength < maxLength){//if the maxlength is greater than truncate constant,then truncate the string 
+					int toBeTruncated = (maxLength - truncatedLength) / 2 ;
+					int modCount = (maxLength - truncatedLength) % 2 ;
+					
+					StringBuilder strBuilder = new StringBuilder();
+					if(modCount == 0){
+					strBuilder.append(termsAndConditions.substring(0, toBeTruncated)).append(TRUN_STRING).append(termsAndConditions.substring((strLength-toBeTruncated),strLength));
+					}else{
+						strBuilder.append(termsAndConditions.substring(0, toBeTruncated)).append(TRUN_STRING).append(termsAndConditions.substring((strLength-(toBeTruncated+modCount)),strLength));
+					}
+					truncatedStr  = strBuilder.toString();
 				}
-				truncatedStr  = strBuilder.toString();
 			}
 		}
-		
 		return truncatedStr;
 	}
 	
